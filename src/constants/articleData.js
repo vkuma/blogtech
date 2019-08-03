0630e608688112a4b3c4e22c9f45727327f2c95b
vkuma <vishnuk9695@gmail.com>
2019-08-03 16:18:14 -0400

    Added the article, "Making a Portable, Compact, Level Warning System Pt.1"

--- src/constants/articleData.js
+++ src/constants/articleData.js
@@ -50,44 +50,14 @@ SITE.articles = [
         link: '#/app/articleblog/article_2'
     }, {
         img: 'assets/article-assets/article3/covers/cover.jpg',
-        date: 'May 1st',
-        title: 'Making a Portable, Compact, Level Warning System Pt.1',
+        date: 'July 3rd, 2019',
+        title: 'Making a Portable, Compact, Level Warning System',
         content: 'Creating a level warning system that detects a gradual change in in position from the transceiver and sends an alert to the receiver device.  ',
         tag: 'Quotes',
         author: 'Vin Kumar',
         avatar: 'assets/article-assets/avatars/vinpic.jpg',
         category: 'Development',
         link: '#/app/articleblog/article_3'
-    }, {
-        img: 'assets/images-demo/assets/600_400-4.jpg',
-        date: 'May 1st',
-        title: 'Be the Change You Wish to See',
-        content: 'Be the change that you wish to see in the world.',
-        tag: 'Quotes',
-        author: 'Mahatma Gandhi',
-        avatar: 'assets/images-demo/avatars/5.jpg',
-        category: 'Development',
-        link: '#/app/articleblog/article_1'
-    }, {
-        img: 'assets/images-demo/assets/600_400-5.jpg',
-        date: 'May 1st',
-        tag: 'Quotes',
-        title: 'Music Matters',
-        content: 'Without music, life would be a mistake',
-        author: 'Friedrich Nietzsche',
-        avatar: 'assets/images-demo/avatars/5.jpg',
-        category: 'Marketing',
-        link: '#/app/articleblog/article_1'
-    }, {
-        img: 'assets/images-demo/assets/600_400-6.jpg',
-        date: 'May 1st',
-        title: 'To know a Man',
-        content: "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
-        tag: 'Quotes',
-        author: 'J.K. Rowling',
-        avatar: 'assets/images-demo/avatars/5.jpg',
-        category: 'Marketing',
-        link: '#/app/articleblog/article_1'
     }
 ];
 
--- src/routes/app/routes/articlepane/components/Products.js
+++ src/routes/app/routes/articlepane/components/Products.js
@@ -2,6 +2,7 @@ import React from 'react';
 import QueueAnim from 'rc-queue-anim';
 import DEMO from 'constants/demoData';
 import SITE from 'constants/articleData';
+import ArticleMarkup from "../../articleblog/routes/article_template/components/Article";
 
 const article = SITE.articles;
 
@@ -20,13 +21,21 @@ class Page extends React.Component {
                     </a>
                   </div>
                   <div className="card__body card-white">
-                    <div className="card__title">
-                      <a href={article.link}>{article.title}</a>
-                      <span>{article.category}</span>
-                    </div>
-                    <div className="card__price">
-                      <span>{article.date}</span>
-                    </div>
+                      <div className='container pr-0'>
+                          <div className='row m-md-2'>
+                              <div className="card__price">
+                                  <span>{article.date}</span>
+                              </div>
+                          </div>
+                          <div className='row'>
+                              <div className="card__title">
+                                  <a href={article.link}>{article.title}</a>
+                                  <span>{article.category}</span>
+                              </div>
+
+                          </div>
+
+                      </div>
                     <div className="divider divider-solid my-4" />
                     <p className="card__desc">{article.content}</p>
                   </div>
